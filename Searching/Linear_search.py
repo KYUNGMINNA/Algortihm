1e75ec572a5c3aa312058b296e452c4f7bd4da10
KYUNGMIN NA <nkm4095@naver.com>
2021-12-13 12:47:07 +0900

    Linear search

--- Searching/Linear_search.py
+++ Searching/Linear_search.py
@@ -1,10 +1,14 @@
-def Linear_search(arr, n, x):
+def Linear_search(arr, n, target):
+
     for i in range(0, n):
-        if (arr[i] == x):
+
+        if (arr[i] == target):
             return i
+
     return -1
 
 arr = [1, 0, 100, 50, 30]
+
 target = 30
 
 result = Linear_search(arr, len(arr), target)
