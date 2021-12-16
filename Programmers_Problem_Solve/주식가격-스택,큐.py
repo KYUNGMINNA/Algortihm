b45fd75f6265065132d08857693a717738184c5a
KYUNGMIN NA <nkm4095@naver.com>
2021-12-16 21:58:19 +0900

    Level2 주식가격 스택,큐

---
+++ Programmers_Problem_Solve/주식가격-스택,큐.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    queue = deque(prices)
+
+    while queue:
+        p = queue.popleft()
+        sec = 0
+        for i in queue:
+            sec += 1
+            if p > i:
+                break
+        answer.append(sec)
+    return print(answer)
+
+prices=[1,2,3,2,3]
+
+solution(prices)
