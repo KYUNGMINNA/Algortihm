e4cc6f1d8c037b5dd42470d723b084e7c93a2122
KYUNGMIN NA <nkm4095@naver.com>
2021-12-18 21:01:48 +0900

    Level2 소수찾기 완전탐색

---
+++ Programmers_Problem_Solve/소수찾기-완전탐색.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+import math
+
+
+def solution(numbers):
+    def numberdetector(number):
+        if number < 2:
+            return False
+        for i in range(2, int(math.sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+        return True
+
+    answers = []
+
+    for i in range(1, len(numbers) + 1):
+        for j in list(permutations(numbers, i)):
+            k = "".join(j)
+            if numberdetector(int(k)):
+                answers.append(int(k))
+    return print(len(set(answers)))
+
+answers="011"
+solution(answers)
