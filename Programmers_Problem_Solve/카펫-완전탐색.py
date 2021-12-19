28076e38aa870c0c18623c5a3a2c44f44b5f2cc6
KYUNGMIN NA <nkm4095@naver.com>
2021-12-19 19:17:26 +0900

    Levle2 카펫 완전탐색

---
+++ Programmers_Problem_Solve/카펫-완전탐색.py
@@ -0,0 +1,15 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow
+    for h in range(3, total + 1):
+        if total % h == 0:
+            w = total//h
+            if (w - 2) * (h - 2) == yellow:
+                answer = [w,h]
+                break
+    return print(answer)
+
+brown=24
+yellow=24
+
+solution(brown,yellow)
