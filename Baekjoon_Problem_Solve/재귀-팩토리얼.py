7fac89c2011338792296693be60b1bc0c2ea7297
kyungmin <nkm4095@naver.com>
2022-01-04 20:13:56 +0900

    재귀 팩토리얼

--- Baekjoon_Problem_Solve/재귀-팩토리얼.py
+++ Baekjoon_Problem_Solve/재귀-팩토리얼.py
@@ -1,11 +1,29 @@
 
 number=int(input())
 
-def factorial(num):
+# def factorial(num):
+#
+#     if num<=1:
+#         return 1
+#     else:
+#         return num*factorial(num-1)
+#
+# print(factorial(number))
 
-    if num<=1:
-        return 1
+def factorial_for(number):
+    result=1
+    if number<=0:
+        while number<0:
+            print("0보다 작은 수 입니다 다시 입력해 주세요")
+            number = int(input())
+        if number>0:
+            for i in range(1, number + 1):
+                result *= i
+        return print(result)
     else:
-        return num*factorial(num-1)
+        for i in range(1, number + 1):
+            result *= i
+    return print(result)
 
-print(factorial(number))
+factorial_for(number)
+
