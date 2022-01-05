86af2abc9d75d894320db4de54a19881dc0bec80
kyungmin <nkm4095@naver.com>
2022-01-05 19:39:21 +0900

    10872 재귀 팩토리얼

--- Baekjoon_Problem_Solve/재귀-팩토리얼.py
+++ Baekjoon_Problem_Solve/재귀-팩토리얼.py
@@ -1,14 +1,14 @@
 
 number=int(input())
 
-# def factorial(num):
-#
-#     if num<=1:
-#         return 1
-#     else:
-#         return num*factorial(num-1)
-#
-# print(factorial(number))
+def factorial(num):
+
+    if num<=1:
+        return 1
+    else:
+        return num*factorial(num-1)
+
+print(factorial(number))
 
 def factorial_for(number):
     result=1
